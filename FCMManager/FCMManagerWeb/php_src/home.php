34cd6c674ab4b920506388bd97629c3a66355f8d
Norbert Fejer <norbertfejer@protonmail.com>
2019-06-03 11:17:49 +0300

    web files removed from repo

--- FCMManager/FCMManagerWeb/php_src/home.php
+++
@@ -1,3 +0,0 @@
-<div class="center">
-    <img id="sapiImg" src="../resources/sapi.png">
-</div>
--- FCMManager/FCMManagerWeb/php_src/login.php
+++
@@ -1,4 +0,0 @@
-<div class="center">
-    <button id="signInBtn" class="btn btn-outline-danger"><i class="fa fa-google"> SignIn with Google</i></button>
-    <img id="sapiImg" src="../resources/sapi.png">
-</div>
